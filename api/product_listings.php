a8d1394ad6e5570f6493631a397377d05a0d8ebe
Connor McCarthy <connor.t.k@icloud.com>
2026-07-18 13:23:55 +0100

    Branch ver 0.2.1 - added get listing id function

--- api/product_listings.php
+++ api/product_listings.php
@@ -7,24 +7,45 @@ class ProductListings {
 
     public function__construct(){
         $instance = Database::getDbInstance();
-        $database = $instnace->getConn();
+        $this->$database = $instnace->getConn();
     }
 
     public function getListings(){
-        
         $query = "SELECT * FROM product_listings";
-        $result = $database->query($query);
+        $result = $this->$database->query($query);
 
-        $sql = "SELECT name,description,price,category,image,dateCreated FROM listings";
+        $sql = "SELECT name,description,price,category,image,dateCreated FROM product_listings";
     }
 
-    public function addListing(){}
+    public function getListingId(){
+        $sql = "SELECT id FROM product_listings WHERE name = ?";
+    }
 
-    public function deleteListing(){}
+    // constructs and executes listing sql statement
+    public function addListing(){
+        $sql = 'INSERT INTO product_listings (name, description, price, category, image, dateCreated) 
+        VALUES (?,?,?,?,?,?)';
+        prepareStmt($sql);
+        executeStmt($stmt){}
+    }
+
+
+    public function deleteListing(){
+        $sql = 'DELETE FROM product_listings WHERE id = ?';
+    }
+
 
 
     public function prepareStmt($stmt){}
 
     public function executeStmt($stmt){}
 }
+
+/** TODO: 
+ * implement prepareStmt function
+ * implement executeStmt functiin 
+ * Add error handling to database conn and queries
+ * Create a way for the relevant data to be used
+ *  */
 ?>
+
