15979a3e41ec85aec8fdc210c1c38857fdda765f
Connor McCarthy <connor.t.k@icloud.com>
2026-07-26 00:08:56 +0100

    Fixed load listings

--- public/index.js
+++ public/index.js
@@ -11,6 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const mainPage = document.getElementById("main_page");
     const homeButtons = document.querySelectorAll("#home_button, #home_button_signup");
 
+    loadListings();
+
     loginButton.addEventListener("click", (e) => {
         e.preventDefault();
         mainPage.style.display = "none";
@@ -66,4 +68,33 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Something went wrong.");
         }
     });
+
+    async function loadListings() {
+        try {
+            const response = await fetch("/api/product_listings.php");
+            const data = await response.json();
+    
+            data.results.forEach(listing => {
+    
+                // creates listing card and places inside of container
+                const card = document.createElement("div");
+                card.className = "listing_card";
+    
+                card.innerHTML = `
+                    <img src="${listing.image}" alt="${listing.name}">
+                    <h2>${listing.name}</h2>
+                    <p>${listing.description}</p>
+                    <p>Category: ${listing.category}</p>
+                    <p>Price: £${listing.price}</p>
+                `;
+    
+                listingContainer.appendChild(card);
+            });
+    
+        } catch (error) {
+            console.error(error);
+            listingContainer.innerHTML = "Unable to load listings";
+        }
+    }
+    
 });
