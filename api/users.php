de99558fa08141dbc34483b17a8b4d116eec226b
Connor McCarthy <connor.t.k@icloud.com>
2026-07-25 17:20:07 +0100

    Branch ver 0.5.0 - Implemented checkUser(), checking for the validity of user login credentials

--- api/users.php
+++ api/users.php
@@ -59,6 +59,34 @@ class users{
         }
     }
 
+    public function checkUser(){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT password FROM `users` WHERE email = ?;";
+        $stmt = $this->prepareStmt($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows === 0) {
+            $this->statuscode = 404;
+            return false;
+        }
+
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row['password'];
+
+        if (password_verify($password, $hashedPassword)) {
+            $this->statuscode = 200;
+            return true;
+        } else {
+            $this->statuscode = 401;
+            return false;
+        }
+    }
+
 
     private function prepareStmt(string $sql): mysqli_stmt|false{
         $stmt = $this->database->prepare($sql);
